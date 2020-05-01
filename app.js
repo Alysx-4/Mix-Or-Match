03fec187ea7a9fa035b362aeff209d28c1458b69
Alyssa <you@example.com>
2020-05-01 18:18:27 +0800

    errors with photo for card-front

--- app.js
+++ app.js
@@ -41,6 +41,7 @@ class MixOrMatch {
         this.ticker = document.getElementById('flips');
         this.audioController = new AudioController();
         this.totalClicks = 0;
+        this.pictures = ['chichen-itza']
     }
     startGame() {
         this.cardToCheck = null;
@@ -49,12 +50,27 @@ class MixOrMatch {
         this.matchedCards = [];
         this.busy = true;
     }
+    
+    getPictureSrc(pictureIndex) {
+        return `./assets/images/${this.pictures[pictureIndex]}.jpg`
+    }
+
+    displayPicture(card, pictureIndex) {
+        console.log(pictureIndex, card);
+        const picture = (document.createElement('img'))
+        picture.src = this.getPictureSrc(pictureIndex)
+        console.log(picture);
+        card.appendChild(picture)
+    }
+
     flipCard(card) {
+        //console.log(card)
         if(this.canFlipCard(card)) {
             this.audioController.flip();
             this.totalClicks++;
             this.ticker.innerText = this.totalClicks;
-            card.classList.add('visible');
+            this.displayPicture(card, 0);
+            //card.classList.add('visible');
         }
     }
     canFlipCard(card) {
@@ -78,7 +94,6 @@ function ready() {
 
     cards.forEach(card => {
         card.addEventListener('click', (e) => {
-            console.log(e.target);
             game.flipCard(e.target);
         })
     })
