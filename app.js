2289de600a99cd9422924309df28030abe248266
Alyssa <you@example.com>
2020-05-02 22:57:12 +0800

    Refactor old implementation

--- app.js
+++ app.js
@@ -1,103 +1,20 @@
-class AudioController {
-    constructor() {
-        this.bgMusic = new Audio('assets/Audio/bgmusic.mp3')
-        this.flipSound=  new Audio('assets/Audio/flip.wav');
-        this.matchSound= new Audio('assets/Audio/match.wav');
-        this.victorySound = new Audio('assets/Audio/victory.wav');
-        this.gameOverSound = new Audio('assets/Audio/gameover.wav');
-        this.bgMusic.volumn = 0.5;
-        this.bgMusic.loop = true;
-
-    }
-    startMusic () {
-        this.bgMusic.play();
-    }
-    stopMusic() {
-        this.bgMusic.pause();
-        this.bgMusic.currenTime = 0;
-    }
-    flip() {
-        this.flipSound.play();
-    }
-    match() {
-        this.matchSound.play();
-    }
-    victory() {
-        this.stopMusic();
-        this.victory.play();
-    }
-    gameOver() {
-        this.stopMusic();
-        this.gameOverSound.play();
-    }
-}
-
-class MixOrMatch {
-    constructor(totalTime, cards) {
-        this.cardsArray = cards;
-        this.totalTime = totalTime;
-        this.timeRemaining = totalTime;
-        this.timer = document.getElementById('time-remaining');
-        this.ticker = document.getElementById('flips');
-        this.audioController = new AudioController();
-        this.totalClicks = 0;
-        this.pictures = ['chichen-itza']
-    }
-    startGame() {
-        this.cardToCheck = null;
-        this.totalClicks = 0;
-        this.timeRemaining = this.totalTime;
-        this.matchedCards = [];
-        this.busy = true;
-    }
-    
-    getPictureSrc(pictureIndex) {
-        return `./assets/images/${this.pictures[pictureIndex]}.jpg`
-    }
-
-    displayPicture(card, pictureIndex) {
-        console.log(pictureIndex, card);
-        const picture = (document.createElement('img'))
-        picture.src = this.getPictureSrc(pictureIndex)
-        console.log(picture);
-        card.appendChild(picture)
-    }
-
-    flipCard(card) {
-        //console.log(card)
-        if(this.canFlipCard(card)) {
-            this.audioController.flip();
-            this.totalClicks++;
-            this.ticker.innerText = this.totalClicks;
-            this.displayPicture(card, 0);
-            //card.classList.add('visible');
-        }
-    }
-    canFlipCard(card) {
-        return true;
-    }
-}
-
 const game = new MixOrMatch(100);
 
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
-    let cards = Array.from(document.getElementsByClassName('card'));
 
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
-            // game.startMusic();
+            game.startMusic();
             game.startGame();
         });
     });
 
-    cards.forEach(card => {
-        card.addEventListener('click', (e) => {
-            game.flipCard(e.target);
-        })
-    })
+    duplicatedCards = cards.slice()
+    cardsArray = cards.concat(duplicatedCards)
 
+    shuffle(cardsArray).forEach((card) => new Card(card))
 }
 
 window.onload = () => {
@@ -105,4 +22,22 @@ window.onload = () => {
     ready();
 }
 
+ function shuffle (array) {
+	var currentIndex = array.length;
+	var temporaryValue, randomIndex;
+
+	// While there remain elements to shuffle...
+	while (0 !== currentIndex) {
+		// Pick a remaining element...
+		randomIndex = Math.floor(Math.random() * currentIndex);
+		currentIndex -= 1;
+
+		// And swap it with the current element.
+		temporaryValue = array[currentIndex];
+		array[currentIndex] = array[randomIndex];
+		array[randomIndex] = temporaryValue;
+	}
+
+	return array;
+};
 
